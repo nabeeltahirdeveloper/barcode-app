f3bc2dc87b76bf6f5087379f9946e21bca847508
nabeeltahirdeveloper <nabeeltahirdeveloper@gmail.com>
2022-05-24 00:27:27 +0500

    fix barcode

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
 import AddEntry from './pages/AddEntry'
+import MainPage from './pages/mainPage'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <AddEntry/>
+      <AddEntry />
+      <MainPage />
     </div>
   )
 }
--- src/pages/mainPage.jsx
+++ src/pages/mainPage.jsx
@@ -1,11 +1,45 @@
 import React from 'react'
+import Barcode from 'react-barcode'
 
-export default function mainPage() {
+
+export default function MainPage() {
   return (
     <div>
+      <h1>Main Page</h1>
+      <div className="table">
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Code</th>
+              <th>Date</th>
+              <th>Font</th>
+              <th>Size</th>
+              <th>Image Link</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>http://cdn.shopify.com/s/files/1/0317/6592/1851/products/allahu-akbar-T-011.jpg?v=1611749422</td>
+            </tr>
+          </tbody>
+        </table>
+        <div style={{
+          display: 'flex',
+          width: '80%',
+        }}>
+
+          <Barcode value="http://cdn.shopify.com/s/files/1/0317/6592/1851/products/allahu-akbar-T-011.jpg?v=1611749422" />
+        </div>
+
+      </div>
 
 
-        
     </div>
   )
 }
