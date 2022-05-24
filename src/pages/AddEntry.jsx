cf10f4f724d531f12640aad2a2c2af58d033cb0d
nabeeltahirdeveloper <nabeeltahirdeveloper@gmail.com>
2022-05-25 00:19:40 +0500

    main page done

--- src/pages/AddEntry.jsx
+++ src/pages/AddEntry.jsx
@@ -1,8 +1,15 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./AddEntry.css";
-import { collection, addDoc } from "firebase/firestore"; 
-import { db , storage } from "../config/config";
-import { getStorage, ref } from "firebase/storage";
+import { collection, addDoc } from "firebase/firestore";
+import { db } from "../config/config";
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+
+const storage = getStorage();
+
+const metadata = {
+    contentType: 'image/jpeg'
+};
+
 
 // const storage = getStorage();
 
@@ -22,90 +29,94 @@ export default function AddEntry() {
             </div>
 
             {/* <form className="entry-form"> */}
-                <label htmlFor="name">Name</label>
-                <input
-                    type="text"
-                    className="form-control"
-                    id="name"
-                    placeholder="Enter name"
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                />
+            <label htmlFor="name">Name</label>
+            <input
+                type="text"
+                className="form-control"
+                id="name"
+                placeholder="Enter name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+            />
 
-                <label htmlFor="code">Code</label>
-                <input
-                    type="text"
-                    className="form-control"
-                    id="code"
-                    placeholder="Enter code"
-                    value={code}
-                    onChange={(e) => setCode(e.target.value)}
-                />
+            <label htmlFor="code">Code</label>
+            <input
+                type="text"
+                className="form-control"
+                id="code"
+                placeholder="Enter code"
+                value={code}
+                onChange={(e) => setCode(e.target.value)}
+            />
 
-                <label htmlFor="date">Date</label>
-                <input
-                    type="date"
-                    className="form-control"
-                    id="date"
-                    placeholder="Enter date"
-                    value={date}
-                    onChange={(e) => setDate(e.target.value)}
-                />
+            <label htmlFor="date">Date</label>
+            <input
+                type="date"
+                className="form-control"
+                id="date"
+                placeholder="Enter date"
+                value={date}
+                onChange={(e) => setDate(e.target.value)}
+            />
 
-                <label htmlFor="font">Font</label>
-                <input
-                    type="number"
-                    className="form-control"
-                    id="font"
-                    placeholder="Enter font size"
-                    value={font}
-                    onChange={(e) => setFont(e.target.value)}
-                />
+            <label htmlFor="font">Font</label>
+            <input
+                type="number"
+                className="form-control"
+                id="font"
+                placeholder="Enter font size"
+                value={font}
+                onChange={(e) => setFont(e.target.value)}
+            />
 
-                <label htmlFor="Size">Size</label>
-                <input
-                    type="number"
-                    className="form-control"
-                    id="Size"
-                    placeholder="Enter size"
-                    value={size}
-                    onChange={(e) => setSize(e.target.value)}
-                />
+            <label htmlFor="Size">Size</label>
+            <input
+                type="number"
+                className="form-control"
+                id="Size"
+                placeholder="Enter size"
+                value={size}
+                onChange={(e) => setSize(e.target.value)}
+            />
 
-                <div className="upload-cont">
-                    <label htmlFor="upload">Upload file:</label>
-                    <input
-                        type="file"
-                        className="form-control"
-                        style={{ alignSelf: "center" }}
-                        id="uplode"
-                        placeholder="Enter uplode"
-                        onChange={(e) => setUploadFile(e.target.files[0])}
-                    />
-                </div>
+            <div className="upload-cont">
+                <label htmlFor="upload">Upload file:</label>
+                <input
+                    type="file"
+                    className="form-control"
+                    style={{ alignSelf: "center" }}
+                    id="uplode"
+                    placeholder="Enter uplode"
+                    onChange={(e) => setUploadFile(e.target.files[0])}
+                />
+            </div>
             {/* </form> */}
-                <button className="submit-btn" onClick={async()=>{
-                    try {
-                        const mountainsRef = ref(storage, `${uploadFile}`);
-                        console.log(uploadFile );
-                        console.log(mountainsRef );
-                        let AllData={
-                            name: name,
-                            code: code,
-                            date: date,
-                            font: font,
-                            size: size,
-                            imageLink: imageLink,
-                        }
-                        const docRef = await addDoc(collection(db, "data"), AllData);
-                        console.log("Document written with ID: ", docRef.id);
-                        alert("Document written with ID: " + docRef.id);
-                      } catch (e) {
-                          alert(e);
-                        console.error("Error adding document: ", e);
-                      }
-                      
-                }}>Submit</button>
+            <button className="submit-btn" onClick={async () => {
+                try {
+                    const storageRef = ref(storage, `images/${uploadFile.name}`);
+                    const uploadTask = uploadBytesResumable(storageRef, uploadFile, metadata);
+                    const downloadURL = await getDownloadURL(uploadTask.snapshot.ref)
+
+                    console.log(downloadURL);
+                    setImageLink(downloadURL);
+                    // console.log(mountainsRef);
+                    let AllData = {
+                        name: name,
+                        code: code,
+                        date: date,
+                        font: font,
+                        size: size,
+                        imageLink: downloadURL,
+                    }
+                    const docRef = await addDoc(collection(db, "data"), AllData);
+                    console.log("Document written with ID: ", docRef.id);
+                    alert("Document written with ID: " + docRef.id);
+                } catch (e) {
+                    alert(e);
+                    console.error("Error adding document: ", e);
+                }
+
+            }}>Submit</button>
         </div>
     );
 }
--- src/pages/mainPage.jsx
+++ src/pages/mainPage.jsx
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./mainPage.css";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../config/config";
 
 
-const ref = React.createRef();
 
 export default function MainPage() {
+    const [AllData, setallData] = useState([])
+    const ref = React.createRef();
+
+    const querySnapshot = async () => {
+        let tempData = [];
+        const docs = await getDocs(collection(db, "data"));
+        docs.forEach((doc) => {
+            let pushData = { ...doc.data(), id: doc.id };
+            tempData.push(pushData);
+        })
+        setallData(tempData);
+        console.log("AllData", tempData);
+    }
+
+
+    useEffect(() => {
+        querySnapshot();
+    }, []);
+
     return (
         <div>
             <div className="table">
@@ -20,17 +40,21 @@ export default function MainPage() {
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            {/* <td>1</td> */}
-                            <td>Mark</td>
-                            <td>Otto</td>
-                            <td>@mdo</td>
-                            <td>@mdo</td>
-                            <td>@mdo</td>
-                            <td>
-                                http://cdn.shopify.com/s/files/1/0317/6592/1851/products/allahu-akbar-T-011.jpg?v=1611749422
-                            </td>
-                        </tr>
+                        {AllData.map((data) => {
+
+                            return (
+                                <tr>
+                                    <td>{data.name}</td>
+                                    <td>{data.code}</td>
+                                    <td>{data.date}</td>
+                                    <td>{data.font}</td>
+                                    <td>{data.size}</td>
+                                    <td>{data.imageLink}</td>
+                                </tr>
+                            )
+                        })
+                        }
+
                     </tbody>
                 </table>
                 {/* <div
@@ -43,16 +67,13 @@ export default function MainPage() {
                     <Barcode value="http://cdn.shopify.com/s/files/1/0317/6592/1851/products/allahu-akbar-T-011.jpg?v=1611749422" />
                     <Pdf targetRef={ref} filename="code-example.pdf">
         {({ toPdf }) => <button onClick={toPdf}>Generate Pdf</button>} */}
-      {/* </Pdf> */}
-      <div >
-        <div ref={ref} style={{
-            display: "none",
-        }}>
-      {/* <Barcode value="http://cdn.shopify.com/s/files/1/0317/6592/1851/products/allahu-akbar-T-011.jpg?v=1611749422" /> */}
-</div>
-      </div>
-                </div>
+                {/* </Pdf> */}
+
+                {/* <Barcode value="http://cdn.shopify.com/s/files/1/0317/6592/1851/products/allahu-akbar-T-011.jpg?v=1611749422" /> */}
+
+
             </div>
+        </div>
         // </div>
     );
 }
