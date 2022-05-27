6be0ced95c682cba4f6659f31eab9aadd46faa77
nabeeltahirdeveloper <nabeeltahirdeveloper@gmail.com>
2022-05-28 00:40:41 +0500

    product page done

--- src/pages/ViewProduct.jsx
+++ src/pages/ViewProduct.jsx
@@ -1,10 +1,9 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useLocation } from 'react-router-dom';
-import Barcode from 'react-barcode';
 import Pdf from "react-to-pdf";
 // import ref from 'react';
 import "./ViewProduct.css";
-
+// import BWIPJS from 'bwip-js/bwipjs';
 
 
 
@@ -15,17 +14,35 @@ export default function ViewProduct(props) {
     const location = useLocation();
     const ref = React.createRef();
 
-
+    // useEffect(() => {
+    //     BWIPJS('myCanvas', {
+    //         bcid: 'code128',
+    //         text: location.state.imageLink,
+    //         scale: 2,
+    //         height: 20,
+    //         includetext: false,
+    //         textxalign: 'center',
+    //     })
+    // }), [location.state.imageLink];
 
     return (
         // <div>{location.state.name}</div>
 
         <div>
+            <center>
 
-            <Pdf targetRef={ref} filename="code-example.pdf">
-                {({ toPdf }) => <button onClick={toPdf}>Generate Pdf</button>}
-            </Pdf>
+                <Pdf targetRef={ref} filename="code-example.pdf">
+                    {({ toPdf }) =>
+                        <button class="download-button" onClick={toPdf}>
+                            <div class="docs"><svg class="css-i6dzq1" stroke-linejoin="round" stroke-linecap="round" fill="none" stroke-width="2" stroke="currentColor" height="20" width="20" viewBox="0 0 24 24"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line y2="13" x2="8" y1="13" x1="16"></line><line y2="17" x2="8" y1="17" x1="16"></line><polyline points="10 9 9 9 8 9"></polyline></svg> Docs</div>
+                            <div class="download">
+                                <svg class="css-i6dzq1" stroke-linejoin="round" stroke-linecap="round" fill="none" stroke-width="2" stroke="currentColor" height="24" width="24" viewBox="0 0 24 24"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path><polyline points="7 10 12 15 17 10"></polyline><line y2="3" x2="12" y1="15" x1="12"></line></svg>
+                            </div>
+                        </button>
+                    }
+                </Pdf>
 
+            </center>
             <div className="pdf" ref={ref} style={{
                 fontSize: `${location.state.size}px`,
                 fontFamily: `${location.state.font}`,
@@ -59,16 +76,12 @@ export default function ViewProduct(props) {
                 </div>
                 <div className="code">
                     <h4>{location.state.code}</h4>
-                    <div style={{
-                        width: "100px",
-                        height: "350px",
-                        display: "flex",
-                        margin: "-60px",
-                        marginRight: "-200px"
+                    <div className='barcode' >
 
-                    }} >
+                        {/* <Barcode value={location.state.imageLink} /> */}
+                        {/* <canvas id="myCanvas" ></canvas> */}
+                        <img className='barcodeImage' src={"http://bwipjs-api.metafloor.com/?bcid=code128&rotate=L&text=" + location.state.imageLink} alt="" />
 
-                        <Barcode value={location.state.imageLink} />
                     </div>
                 </div>
 
